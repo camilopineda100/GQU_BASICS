f003108b1cec18dc2b5ea12d3c40d5130fefcc24
camilopineda100 <camilopineda100@gmail.com>
2021-04-10 11:52:32 -0500

    feat: arguments to getting an array part 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { GraphQLServer } from 'graphql-yoga'
 import axios from 'axios'
 
+const dbUrl = 'http://localhost:3000'
+
 const server = new GraphQLServer({
     typeDefs: `
         type Query {
             agent(id: ID!): User
-            agents: [User]
+            agents(name: String, age: Int): [User]
+            cars: [String]
+            msg(values: [String!]!): String
         }
 
         type User {
@@ -19,12 +23,24 @@ const server = new GraphQLServer({
     resolvers: {
         Query: {
             agent: async (parent, args, context, info) => {
-                const response = await axios.get(`http://localhost:3000/users/${args.id}`) // getting data from a Rest API
+                const response = await axios.get(`${dbUrl}/users/${args.id}`) // getting data from Fake Rest API
                 return response.data
             },
-            agents: async () => {
-                const response = await axios.get('http://localhost:3000/users') // getting data from a Rest API
+            agents: async (parent, args, context, info) => {
+                const name = args.name != null ? `name=${args.name}` : ''
+                const age = args.age != null ? `age=${args.age}` : ''
+
+                const response = await axios.get(`${dbUrl}/users?${name}&${age}`) // getting data from Fake Rest API 
                 return response.data
+            },
+            cars: async () => {
+                return ['mazda', 'ford']
+            },
+            msg: async (parent, args, context, info) => {
+                if(args.values.length === 0) {
+                    return 'Sorry no values'
+                }
+                return `Hello ${args.values[0]} ${args.values[1]}`
             }
         }
     }
